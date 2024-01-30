58db4f642c64fdcbbf81db3298a057da7fc97a44
dangayaudio <153900798+dangayaudio@users.noreply.github.com>
2024-01-30 16:07:23 +0700

    NodeJS

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,21 @@
+const connection = require('../config/database')
+
+
+
 const getHomepage = (req, res) => {
-    res.send('Love Lian');
+    //simple query
+    let users = [];
+
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results
+            console.log("check results:", results);
+            console.log('check users:', users);
+            res.send(JSON.stringify(users));
+        }
+    )
+
 }
 const getLian = (req, res) => {
     res.render('sample.ejs');
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./route/web')
+const connection = require('./config/database')
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -12,6 +13,18 @@ configViewEngine(app);
 
 //khai báo route
 app.use('/', webRoutes);
+
+//create database
+
+
+//simple query
+// connection.query(
+//     'select * from Users u',
+//     function (err, results, fields) {
+//         console.log("check results:", results);
+//     }
+// )
+
 
 app.listen(port, hostmane, () => {
     console.log(`Example app listening on port ${port}`)
